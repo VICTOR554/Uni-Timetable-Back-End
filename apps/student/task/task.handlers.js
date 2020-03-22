8fbc3552acb29818c73780e9166194174bcecd9c
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2020-03-22 14:33:33 +0000

    Created getAllTasks_onSchedule, getAllTasks_flagged, getAllTasks_completed and getAllTasks_overdue. Also modified editTask, createTask

--- apps/student/task/task.handlers.js
+++ apps/student/task/task.handlers.js
@@ -1,55 +1,127 @@
 const models = require('../../../models/model')
+const moment = require('moment')
 
-
-const getAllTasks = function(req, res){
-    models.Task.find().then(d => {
-        res.send(d)
-    }
-    ).catch(e => {
-        console.log(e)
-        res.send(e)
+const getAllTasks_onSchedule = function(req, res){
+    console.log('oooo')
+    date = +moment().format('X')
+    console.log(date)
+    models.Task.find({ student_number: req.student_number, is_completed: false, due_date_time: {$gt: date}})
+    .sort( { create_date_time: -1 } )
+    .then(d => {
+      res.send(d);
+      // console.log(d)
     })
+    .catch(e => {
+      console.log(e);
+      res.send(e);
+    });
 }
 
-// dont know what to replace username with
-const getOneTask = function(req, res){
-    models.Task.findOne({ username: req.params.username, _id: req.params._id }).then(d => {
-        res.send(d)
-    }
-    ).catch(e => {
-        console.log(e)
-        res.send(e)
+const getAllTasks_completed = function(req, res){
+    models.Task.find({ student_number: req.student_number, is_completed: true }).sort( { created_date_time: -1 } )
+    .then(d => {
+      res.send(d);
+      // console.log(d)
     })
+    .catch(e => {
+      console.log(e);
+      res.send(e);
+    });
+}
+
+const getAllTasks_flagged = function(req, res){
+    models.Task.find({ student_number: req.student_number, is_flagged: true} ).sort( { due_date_time: -1 } )
+    .then(d => {
+      res.send(d);
+      // console.log(d)
+    })
+    .catch(e => {
+      console.log(e);
+      res.send(e);
+    });
+}
+
+const getAllTasks_overdue = function(req, res){
+    date = moment(Date.now(), 'X')
+    models.Task.find({ student_number: req.student_number, is_completed: false, due_date_time: {$lt: date} }).sort( { due_date_time: -1 } )
+    .then(d => {
+      res.send(d);
+      // console.log(d)
+    })
+    .catch(e => {
+      console.log(e);
+      res.send(e);
+    });
+}
+
+
+const getOneTask = function(req, res){
+    models.Task.findOne({
+        student_number: req.student_number,
+        _id: req.params.id
+      })
+      .then(d => {
+        if (d){
+          res.send(d);
+          console.log(d);
+        } else {
+          res.json({text: 'nope'})
+          console.log('nope')
+        }
+      }) 
+      .catch(e => {
+        console.log(e);
+        res.send(e);
+      });
 }
 
 const createTask = function(req, res){
-    models.Task.create(req.body).then(doc =>{
-        console.log('new task:', doc)
-        res.send(doc)
-    }).catch(e =>{
-        console.log(e)
+    console.log('what it do')
+    student = {student_number: req.student_number}
+
+    models.Task.create({ ...req.body, ...student })
+    .then(doc => {
+      console.log("new note:", doc);
+      res.status(200).send(doc);
     })
+    .catch(e => {
+      console.log(e);
+      res.status(400).send(e);
+    });
+    
 }
 
 const editTask = function(req, res){
-    models.Task.updateOne({_id: req.params.id}).then(d=>{
-        res.status(200).send(d)
-    }).catch(e=>{res.status(400).send(e)})
+    console.log('nope', req.student_number)
+    console.log(req.body)
+    models.Task.updateOne({_id: req.params.id}, req.body)
+    .then(doc => {
+      console.log(doc);
+      res.status(200).send(doc);
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
 }
 
 const deleteTask = function(req, res){
-    models.Task.deleteOne(req.body).then(doc =>{
-        console.log('new task:', doc)
-        res.send(doc)
-    }).catch(e =>{
-        console.log(e)
+    models.Task.deleteOne({_id: req.params.id, student_number: req.student_number})
+    .then(doc => {
+      console.log("delete task:", doc);
+      res.send(doc);
     })
+    .catch(e => {
+      console.log(e);
+    });
 
 }
 
 
 module.exports = {
-    getAllTasks,
+    getAllTasks_onSchedule,
+    getAllTasks_completed,
+    getAllTasks_flagged,
+    getAllTasks_overdue,
     getOneTask,
     createTask,
     editTask,
--- apps/student/task/task.js
+++ apps/student/task/task.js
@@ -4,12 +4,16 @@ const router = express.Router()
 const handlers = require('./task.handlers')
 
 router.post('/new', handlers.createTask)
-router.get('/:id', handlers.getOneTask)
-router.get('/', handlers.getAllTasks)
+router.get('/onschedule', handlers.getAllTasks_onSchedule)
+router.get('/completed', handlers.getAllTasks_completed)
+router.get('/flagged', handlers.getAllTasks_flagged)
+router.get('/overdue', handlers.getAllTasks_overdue)
+router.get('/one/:id', handlers.getOneTask)
+
 router.put('/edit/:id', handlers.editTask)
 router.delete('/delete/:id', handlers.deleteTask)
 
 
 
 
-module.exports = router
+module.exports = router 
